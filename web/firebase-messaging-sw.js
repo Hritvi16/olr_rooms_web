632cf7cbf51a77f28cd3f87042900cd66c4dad12
hritvigajiwala <hritvi16gajiwala@gmail.com>
2022-06-25 14:41:12 +0530

    Latest

---
+++ web/firebase-messaging-sw.js
@@ -0,0 +1,33 @@
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js");
+
+if (!firebase.apps.length) {
+   firebase.initializeApp({
+     apiKey: 'AIzaSyAmw1PjLK82peDwBi-a1pxLSBLYkXnXHPk',
+     appId: '1:191908340162:web:b7d897f1db846fe018b65b',
+     messagingSenderId: '191908340162',
+     projectId: 'olrrooms-1c565',
+     authDomain: 'olrrooms-1c565.firebaseapp.com',
+     storageBucket: 'olrrooms-1c565.appspot.com',
+     measurementId: 'G-3RXQNZ79RW',
+   });
+}else {
+   firebase.app(); // if already initialized, use that one
+}
+
+
+// Necessary to receive background messages:
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage(function(payload) {
+    console.log('Received background message ', payload);
+
+    const notificationTitle = payload.notification.title;
+    const notificationOptions = {
+      body: payload.notification.body,
+    };
+
+    self.registration.showNotification(notificationTitle,
+      notificationOptions);
+  });
